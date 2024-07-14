1c02920d7c600217fc241b9061fedab5dd8d947f
CK-Explorer <121228064+CK-Explorer@users.noreply.github.com>
2024-07-15 03:18:24 +0800

    Added function to determine the least rotation
    Implemented a new function that explore all the possible rotations deviated from the ref. frame and produce a single configuration with the least rotation angle.

---
+++ include/ellipsoid_fit/ellipsoid/minRotFinder.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+namespace minRotFinder {
+
+/**
+ * @brief Rearrange the eigenvectors with their respective eigenvalues for the smallest 
+ * angle deviated from the ref. frame.
+ * 
+ * This function first permutates the eigenvalues with their respective eigenvalues (3! = 
+ * 6 arrangements), then assigns all possible signs to each permutated eigenvectors set 
+ * (4 possibilites) while maintaining the determinant of eigenvector matrix to be +1.
+ * 
+ * Subsequently, using Rodrigues' rotation formula, the function computes the rotational 
+ * angles based on the rotational matrices formed from each configuration of eigenvectors.
+ * 
+ * Lastly, the two input parameters are overwrote with the configuration which possesses 
+ * the smallest rotational angle deviated from the ref. frame.
+ * 
+ * @param  eval 3D vector containing eigenvalues
+ * @param  evec_column 3x3 Matrix containing eigenvectors arranged in column vectors
+ * 
+ * @note The order of eigenvectors (column) in `evec_column` is the same as the eigenvalues 
+ * in `eval`.
+ * 
+ */
+void find_min_rotation(Eigen::Vector3d& eval, Eigen::Matrix3d& evec_column);
+
+} // namespace minRotFinder
