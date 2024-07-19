b8746e218870f7f3ad72a8f8587b6bd84908f560
CK-Explorer <121228064+CK-Explorer@users.noreply.github.com>
2024-07-20 03:05:04 +0800

    Corrections on test files

--- test/fit/main.cpp
+++ test/fit/main.cpp
@@ -54,7 +54,7 @@ int main(int argc, char const* argv[]) {
                 ellipsoid::fit(points, ellipsoid::EllipsoidType::Sphere);
         } else {
             identified_parameters =
-                ellipsoid::fit(points);
+                ellipsoid::fit(points, ellipsoid::EllipsoidType::Arbitrary);
         }
 
         check_vector3d(identified_parameters.center, parameters.center,
