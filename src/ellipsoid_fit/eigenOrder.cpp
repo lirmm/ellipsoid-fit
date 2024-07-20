2176896f4300983a48eec9467ee4a3b14c673831
CK-Explorer <121228064+CK-Explorer@users.noreply.github.com>
2024-07-21 02:17:40 +0800

    Removed some parts

--- src/ellipsoid_fit/eigenOrder.cpp
+++ src/ellipsoid_fit/eigenOrder.cpp
@@ -1,21 +1,3 @@
-/*      File: eigenOrder.cpp
-*       This file is part of the program ellipsoid-fit
-*       Program description : Ellipsoid fitting in C++ using Eigen. Widely inspired by https://www.mathworks.com/matlabcentral/fileexchange/24693-ellipsoid-fit
-*       Copyright (C) 2018-2021 -  Benjamin Navarro (LIRMM) CK-Explorer (). All Right reserved.
-*
-*       This software is free software: you can redistribute it and/or modify
-*       it under the terms of the LGPL license as published by
-*       the Free Software Foundation, either version 3
-*       of the License, or (at your option) any later version.
-*       This software is distributed in the hope that it will be useful,
-*       but WITHOUT ANY WARRANTY without even the implied warranty of
-*       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*       LGPL License for more details.
-*
-*       You should have received a copy of the GNU Lesser General Public License version 3 and the
-*       General Public License version 3 along with this program.
-*       If not, see <http://www.gnu.org/licenses/>.
-*/
 #include <ellipsoid/eigenOrder.h>
 
 void eigenOrder::leastRotationAngle(Eigen::Vector3d& eval, Eigen::Matrix3d& evec_column)
